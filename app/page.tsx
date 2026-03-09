238d3347b3b89777d13a46d2e93864951db951f1
Zack289 <ozack911@gmail.com>
2026-03-09 18:26:10 +0545

    Refactor global styles and add Explore button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
+import ExploreBtn from "@/components/ExploreBtn"
 
 const Home = () => {
   return (
     <section>
+      {/* eslint-disable-next-line react/no-unescaped-entities */}
       <h1 className="text-center">The Hub for Every Dev <br /> Event You Can't Miss</h1>
+      <p className="text-center mt-5">Hackathons, Meetups, and Conferences, All in One Place</p>
+
+      <ExploreBtn />
     </section>
   )
 }
